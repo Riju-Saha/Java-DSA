1fd91d123039802e6e16019ce07390100d78c2d7
Riju-Saha <21.sahasoumyajit@gmail.com>
2025-04-05 00:36:43 +0530

    daa

--- DAA practise/prims_algorithm.C
+++ DAA practise/prims_algorithm.C
@@ -1,93 +1,82 @@
-#include <stdio.h>
+#include<stdio.h>
 
 struct Edge {
     int src, dest, wt;
 };
 
-void prims(struct Edge edges[], int v, int e) {
-  int target = v-1;   //3
-  int cost = 0;
-  
-  for(int i=0; i<e; i++) {  //sorting in ascending order respect to weight.
-    for(int j=i+1; j<e; j++) {
-      if(edges[i].wt>edges[j].wt) {
-        int temp_src = edges[i].src;
-        int temp_dest = edges[i].dest;
-        int temp_wt = edges[i].wt;
-        
-        edges[i].src = edges[j].src;
-        edges[i].dest = edges[j].dest;
-        edges[i].wt = edges[j].wt;
-        
-        edges[j].src = temp_src;
-        edges[j].dest = temp_dest;
-        edges[j].wt = temp_wt;
-      }
-    }
-  }
-  
-  for (int i=0;i<e; i++) {
-    printf("%d   %d   %d \n", edges[i].src, edges[i].dest, edges[i].wt);
-  }
-
-  int root_src = edges[0].src;  //2
-  int root_dest = edges[0].dest; // 3 
-  int root_wt = edges[0].wt; // 4
-  cost += root_wt;  //manually add krlm karon acsending a eta lowest.
-  target--;
-  
-  for(int i = 1; i<e; i++) {
-    if (target!=0) {
-      if(root_dest == edges[i].src || root_src == edges[i].dest) {   //karur start karur end hbena and vice versa
-        continue; 
-      } else {
-        cost += edges[i].wt;
-        printf("edge (%d -->  %d  =  %d \n)", edges[i].src, edges[i].dest, edges[i].wt);
-        target--;
-      }
+void prims(struct Edge edges[], int ver, int edg) {
+  int sum = 0;
+  int i =0;
+  int fwt = 0;
+  while(i<7) {
+    if(edges[i].src == edges[i+1].src) {
+        if (edges[i].wt < edges[i+1].wt) {
+          fwt += edges[i].wt ;
+        } else {
+          fwt += edges[i+1].wt;
+        };
+      i+=2;
     } else {
-      break;
+      fwt += edges[i].wt;
+      i++;
     }
+    
   }
   
-  printf("MST cost is %d ", cost);
-
+  printf("min cost is  %d", fwt);
 }
 
 int main() {
-  int ver = 4;
-  int edg = 5;
-  struct Edge edges[edg];
+  int ver = 5;
+  int edg = ver-1;
+  struct Edge edges[7];
   
-  edges[0].src = 0;
-  edges[0].dest = 1;
-  edges[0].wt = 10;
   
-  edges[1].src = 0;
-  edges[1].dest = 2;
-  edges[1].wt = 6;
+  edges[0].src = 1;
+  edges[0].dest = 2;
+  edges[0].wt = 2;
   
-  edges[2].src = 0;
+  edges[1].src = 1;
+  edges[1].dest = 3;
+  edges[1].wt = 3;
+  
+  edges[2].src = 2;
   edges[2].dest = 3;
-  edges[2].wt = 5;
+  edges[2].wt = 1;
   
-  edges[3].src = 1;
-  edges[3].dest = 3;
-  edges[3].wt = 15;
+  edges[3].src = 2;
+  edges[3].dest = 4;
+  edges[3].wt = 4;
   
-  edges[4].src = 2;
-  edges[4].dest = 3;
-  edges[4].wt = 4;
+  edges[4].src = 3;
+  edges[4].dest = 4;
+  edges[4].wt = 5;
   
-
+  edges[5].src = 3;
+  edges[5].dest = 5;
+  edges[5].wt = 6;
+  
+  edges[6].src = 4;
+  edges[6].dest = 5;
+  edges[6].wt = 7;
+  
   prims(edges, ver, edg);
+  
   return 0;
+  
 }
 
 
+
 /*
-prims algorith 
-edges = vertices -1 r shetai thkte hbe in every mst
-jst plot the points. and connect with those vertices whose weights are lowest, ascending order. and how many veritces to join? no.of edges calculated abvoe
+
+for prims it it to choose the minimum weight from the source vertex
+
+the only trick is to implement while loop (i<total number of edges) and if consecutive src is same then we compare their weights 
+increase the i +2 to avoid repeat checking. 
+
+and if consecutive src is not same we simply add the weight. 
+
+taking the smallest edges gurantees that cycle will not form
 
 */
